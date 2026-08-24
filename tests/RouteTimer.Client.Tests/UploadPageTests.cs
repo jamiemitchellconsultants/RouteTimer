8873a26375f813913c1e59ea57e92e5321afecc3
Jamie Mitchell <jamie@jjmconsultants.com>
2026-08-24 07:54:52 +0100

    feat: add FIT and GPX upload selectors

---
+++ tests/RouteTimer.Client.Tests/UploadPageTests.cs
@@ -0,0 +1,27 @@
+using Bunit;
+using RouteTimer.Client.Pages;
+
+namespace RouteTimer.Client.Tests;
+
+public sealed class UploadPageTests : BunitContext
+{
+    [Fact]
+    public void Training_accepts_multiple_fit_files()
+    {
+        var cut = Render<Training>();
+        var input = cut.Find("input[type=file]");
+
+        Assert.Equal(".fit", input.GetAttribute("accept"));
+        Assert.NotNull(input.GetAttribute("multiple"));
+    }
+
+    [Fact]
+    public void Predictions_accepts_one_gpx_file()
+    {
+        var cut = Render<Predictions>();
+        var input = cut.Find("input[type=file]");
+
+        Assert.Equal(".gpx", input.GetAttribute("accept"));
+        Assert.Null(input.GetAttribute("multiple"));
+    }
+}
