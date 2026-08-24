820fcb9af58c148e89504b1db8c8616e2224d90b
Jamie Mitchell <jamie@jjmconsultants.com>
2026-08-24 08:16:20 +0100

    feat: submit FIT training uploads from client

--- src/RouteTimer.Client/Program.cs
+++ src/RouteTimer.Client/Program.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using RouteTimer.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Keycloak", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+});
+builder.Services.AddScoped(sp =>
+{
+    var handler = sp.GetRequiredService<AuthorizationMessageHandler>()
+        .ConfigureHandler(authorizedUrls: [builder.HostEnvironment.BaseAddress]);
+    return new HttpClient(handler) { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 });
 
 await builder.Build().RunAsync();
--- tests/RouteTimer.Client.Tests/UploadPageTests.cs
+++ tests/RouteTimer.Client.Tests/UploadPageTests.cs
@@ -1,10 +1,13 @@
 using Bunit;
+using Microsoft.Extensions.DependencyInjection;
 using RouteTimer.Client.Pages;
 
 namespace RouteTimer.Client.Tests;
 
 public sealed class UploadPageTests : BunitContext
 {
+    public UploadPageTests() => Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://example.test/") });
+
     [Fact]
     public void Training_accepts_multiple_fit_files()
     {
